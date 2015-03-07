4ac508cfb273ce85646c9ddfce07722e4be5d44c
Behroz Saadat <bmsaadat@uwaterloo.ca>
2015-03-07 13:51:23 -0500

    Fixed database connection settings

--- src/java/ece356/UserDBAO.java
+++ src/java/ece356/UserDBAO.java
@@ -3,10 +3,10 @@ package ece356;
 import java.sql.*;
 
 public class UserDBAO {
-    public static final String url = "jdbc:mysql://snorkel.uwaterloo.ca:3306";
+    public static final String url = "jdbc:mysql://eceweb.uwaterloo.ca:3306";
     //public static final String url = "jdbc:mysql://eceweb.uwaterloo.ca:3306/";
-    public static final String user = "1user_ece356_test";
-    public static final String pwd = "user_ece356_test";
+    public static final String user = "user_bmsaadat";
+    public static final String pwd = "user_bmsaadat";
     
     public static void testConnection()
             throws ClassNotFoundException, SQLException {
