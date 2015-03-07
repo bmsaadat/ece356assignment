f7b0be0a7346dd6926682ff4c94c7e619a7d9cb5
Behroz Saadat <bmsaadat@uwaterloo.ca>
2015-03-07 14:56:06 -0500

    Changed some stuff

--- src/java/ece356/UserDBAO.java
+++ src/java/ece356/UserDBAO.java
@@ -5,16 +5,57 @@ import java.sql.*;
 public class UserDBAO {
     public static final String url = "jdbc:mysql://eceweb.uwaterloo.ca:3306";
     //public static final String url = "jdbc:mysql://eceweb.uwaterloo.ca:3306/";
-    public static final String user = "user_bmsaadat";
-    public static final String pwd = "user_bmsaadat";
+    public static final String nid = "bmsaadat";
+    public static final String user = "user_" + nid;
+    public static final String pwd = "user_" + nid;    
     
-    public static void testConnection()
+    public static Connection getConnection()
             throws ClassNotFoundException, SQLException {
-        Statement stmt;
-        Connection con;
         Class.forName("com.mysql.jdbc.Driver");
-        con = DriverManager.getConnection(url, user, pwd);
-        stmt = con.createStatement();
-        con.close();
+        Connection con = DriverManager.getConnection(url, user, pwd);
+        Statement stmt = null;
+        try {
+            con.createStatement();
+            stmt = con.createStatement();
+            stmt.execute("USE ece356db_" + nid);
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+        return con;
     }
+    
+    public static DoctorData queryDoctor(String userName)
+           throws ClassNotFoundException, SQLException {
+       Connection con = null;
+       PreparedStatement pstmt = null;
+       DoctorData ret;
+       System.out.println("TESTING");
+       try {
+           con = getConnection();
+
+           /* Build SQL query */
+           String query = "SELECT * FROM doctorView where username = " + userName;
+           
+           pstmt = con.prepareStatement(query);          
+
+           ResultSet resultSet;
+           resultSet = pstmt.executeQuery();
+
+           ret = new DoctorData();
+           ret.userName = resultSet.getString("username");
+           ret.firstName = resultSet.getString("first_name");
+           ret.lastName = resultSet.getString("last_name");
+
+           return ret;
+       } finally {
+           if (pstmt != null) {
+               pstmt.close();
+           }
+           if (con != null) {
+               con.close();
+           }
+       }
+   }
 }
