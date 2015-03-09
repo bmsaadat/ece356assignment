dff95f717e874b97fcda36fac7cfea822b64c4cc
Behroz Saadat <bmsaadat@uwaterloo.ca>
2015-03-09 12:25:56 -0400

    - Added patientData - Added patient search views - Refactored some code for doctor

---
+++ src/java/ece356/PatientData.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ece356;
+import java.util.*;
+/**
+ *
+ * @author behrozsaadat
+ */
+public class PatientData extends UserData{
+    String city;
+    String state;
+    int numberOfReviews;
+    Date lastReviewDate;
+    
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getNumberOfReviews() {
+        return numberOfReviews;
+    }
+
+    public void setNumberOfReviews(int numberOfReviews) {
+        this.numberOfReviews = numberOfReviews;
+    }
+
+    public Date getLastReviewDate() {
+        return lastReviewDate;
+    }
+
+    public void setLastReviewDate(Date lastReviewDate) {
+        this.lastReviewDate = lastReviewDate;
+    }
+    
+}
---
+++ web/patientSearchResultsView.jsp
@@ -0,0 +1,17 @@
+<%--
+    Document   : patientProfileView
+    Created on : Mar 9, 2015, 11:55:00 AM
+    Author     : behrozsaadat
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Patient Search Results</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
