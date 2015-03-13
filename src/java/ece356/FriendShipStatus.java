ac02a0301be78ba4227a900d5535080e227b16b7
Behroz Saadat <bmsaadat@uwaterloo.ca>
2015-03-13 12:37:31 -0400

    Added FriendShipStatus file

---
+++ src/java/ece356/FriendShipStatus.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ece356;
+
+/**
+ *
+ * @author behrozsaadat
+ */
+   
+public enum FriendShipStatus {
+    ALREADY_FRIENDS, WAITING_FOR_ACCEPT, REQUEST_SENT, FRIENDSHIP_ESTABLISHED
+}
